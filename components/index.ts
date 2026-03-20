f8987cdfcfb690416fb80f0db784f4e1a29da387
Mattia Mularoni <mattia.mularoni@gmail.com>
2026-03-20 14:39:03 +0100

    Fixes
    Removed duplicated providers, fixed strava scopes,  migrated middleware to proxy

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,5 @@
 export * from "./ActivityCard";
 export * from "./ConnectStrava";
 export * from "./ErrorBoundary";
-export * from "./Providers";
 export * from "./ThemeProvider";
 export * from "./ThemeToggle";
